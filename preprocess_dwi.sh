c2e2f0aa469ce9383442fb4a28b27ba95468f602
FreekvdHeijden17 <fkheijden@gmail.com>
2025-04-23 14:33:51 +0200

    Update preprocess_dwi.sh

---
+++ preprocess_dwi.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## Run MRtrix3 Docker container
+docker run -it --rm -v "C:\Users\u361338\OneDrive - Tilburg University\Documenten\MRITRIX":/data mrtrix3/mrtrix3
+
+# Inside Docker (in command line):
+
+# Go to subject folder
+cd /data/Subject2
+
+# Convert DWI NIfTI to MRtrix format (.mif)
+mrconvert sub-CC110045_dwi.nii.gz sub-CC110045_dwi.mif -fslgrad sub-CC110045_dwi.bvec sub-CC110045_dwi.bval
+
+# Rename files for clarity
+mv sub-CC110045_dwi.bvec Subject2.bvec
+mv sub-CC110045_dwi.bval Subject2.bval
+mv sub-CC110045_dwi.nii.gz Subject2_dwi.nii.gz
+mv sub-CC110045_dwi.json Subject2_dwi.json
+
+# Check image information
+mrinfo Subject2_dwi.mif
+
+# Verify number of volumes
+mrinfo -size Subject2_dwi.mif | awk '{print $4}'
+awk '{print NF; exit}' Subject2.bvec
+awk '{print NF; exit}' Subject2.bval
+
+# Denoising
+dwidenoise Subject2_dwi.mif Subject2_den.mif -noise noise.mif
+
+# Remove Gibbs ringing 
+mrdegibbs Subject2_den.mif Subject2_den_unr.mif
+
+# Motion and Eddy current correction (without reverse phase encoding images)
+dwifslpreproc Subject2_den.mif Subject2_den_preproc.mif -nocleanup -pe_dir AP -rpe_none -eddy_options "--slm=linear --data_is_shelled"
