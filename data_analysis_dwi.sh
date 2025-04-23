685bf8f6898930a55c350c286f5dfca93f6b6e1c
FreekvdHeijden17 <fkheijden@gmail.com>
2025-04-23 16:45:40 +0200

    Update

---
+++ data_analysis_dwi.sh
@@ -0,0 +1,29 @@
+# Initiate algoritm to deconvolce the fiber orientation distributions (FODs)
+dwi2response dhollander Subject2_den_preproc_unbiased.mif wm.txt gm.txt csf.txt -voxels voxels.mif
+
+# Estimate white matter, gray matter, and CSF fiber orientation distributions using multi-tissue CSD from preprocessed DWI data.
+dwi2fod msmt_csd Subject2_den_preproc_unbiased.mif -mask mask.mif wm.txt wmfod.mif gm.txt gmfod.mif csf.txt csffod.mif
+
+# Extract the first volume of the WM FOD image and concatenate it with CSF and GM FODs to create a 3-tissue volume fraction image.
+mrconvert -coord 3 0 wmfod.mif - | mrcat csffod.mif gmfod.mif - vf.mif
+
+# Perform multi-tissue FOD intensity normalization across WM, GM, and CSF using a brain mask.
+mtnormalise wmfod.mif wmfod_norm.mif gmfod.mif gmfod_norm.mif csffod.mif csffod_norm.mif -mask mask.mif
+
+
+# The anatomical image first needs to be converted to MRtrix format
+mrconvert sub-CC110045_T1w.nii t1.mif
+
+# Segment the anatomical image into the 5 tissue types
+5ttgen fsl T1.mif 5tt_nocoreg.mif
+
+# Extract b=0 volumes from the DWI data and compute the mean across them, saving the result as `mean_b0.mif`.
+dwiextract Subject2_den_preproc_unbiased.mif - -bzero | mrmath - mean mean_b0.mif -axis 3
+
+# Move the `5tt_nocoreg.mif` file to the current directory and convert `mean_b0.mif` and `5tt_nocoreg.mif` to NIfTI format (`.nii.gz`).
+mv ../anat/5tt_nocoreg.mif .
+mrconvert mean_b0.mif mean_b0.nii.gz
+mrconvert 5tt_nocoreg.mif 5tt_nocoreg.nii.gz
+
+# Extract the first volume (index 0) from the `5tt_nocoreg.nii.gz` file and save it as `5tt_vol0.nii.gz`.
+fslroi 5tt_nocoreg.nii.gz 5tt_vol0.nii.gz 0 1
