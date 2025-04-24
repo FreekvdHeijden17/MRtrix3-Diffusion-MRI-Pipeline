f9eb5b3fa4c0385a7a7e61cf6295531e9d0630a0
Freek van der Heijden <F.F.J.vdrHeijden@tilburguniversity.edu>
2025-04-24 09:29:53 +0200

    Update data_analysis_dwi.sh

--- data_analysis_dwi.sh
+++ data_analysis_dwi.sh
@@ -36,11 +36,20 @@ flirt -in mean_b0.nii.gz -ref 5tt_vol0.nii.gz -interp nearestneighbour -dof 6 -o
 # Convert the transformation matrix from FSL format into MRtrix-readable format using `transformconvert`.
 transformconvert diff2struct_fsl.mat mean_b0.nii.gz 5tt_nocoreg.nii.gz flirt_import diff2struct_mrtrix.txt
 
+# Applies the inverse of a linear transformation (diff2struct_mrtrix.txt) to the 5-tissue-type image (5tt_nocoreg.mif) to align it with diffusion space
 mrtransform 5tt_nocoreg.mif -linear diff2struct_mrtrix.txt -inverse 5tt_coreg.mif
 
+# This command extracts the gray matter–white matter interface (GMWMI) from the coregistered 5-tissue-type image (`5tt_coreg.mif`) and saves it as a seed mask (`gmwmSeed_coreg.mif`) for anatomically constrained tractography.
 5tt2gmwmi 5tt_coreg.mif gmwmSeed_coreg.mif
 
+# This command generates 10 million streamlines using anatomically constrained tractography (`-act`), backtracking for better termination (`-backtrack`), seeding from the gray matter–white matter interface (`-seed_gmwmi`), and several other parameters (e.g., max length, FOD cutoff, threading), saving the output to `tracks_10M.tck`.
 tckgen -act 5tt_coreg.mif -backtrack -seed_gmwmi gmwmSeed_coreg.mif -nthreads 8 -maxlength 250 -cutoff 0.06 -select 10000000 wmfod_norm.mif tracks_10M.tck
 
 # To counter-balance this overfitting, the command tcksift2 will create a text file containing weights for each voxel in the brain:
 tcksift2 -act 5tt_coreg.mif -out_mu sift_mu.txt -out_coeffs sift_coeffs.txt -nthreads 8 tracks_10M.tck wmfod_norm.mif sift_1M.txt
+
+# Sets the `SUBJECTS_DIR` environment variable to the current working directory (returned by the `pwd` command), telling FreeSurfer where to look for or save subject data.
+SUBJECTS_DIR='pwd'
+
+# runs FreeSurfer’s full cortical reconstruction and segmentation pipeline (-all) on the input T1-weighted image, assigning the results to a subject directory named Sub-CON02_recon.
+recon-all -i sub-CC110045_T1w.nii -s sub-CON02_recon -all
